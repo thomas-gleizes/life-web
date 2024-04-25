121ece47b54cef392642607f012643ee6902868b
Thomas <thomasgleizes34@gmail.com>
2024-04-25 22:36:29 +0200

    start of V2 of game of life with infinite size

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-
-import App from "./App.tsx"
-import "./styles/global.css"
-
-const root = document.getElementById("root") as HTMLElement
-
-createRoot(root).render(<App />)
